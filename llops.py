074821c4672e9ef02ecf30395ba019e98d07c650
Marky376 <mmutuamark@gmail.com>
2023-11-28 09:50:42 +0300

    A commit on some loops in python

---
+++ llops.py
@@ -0,0 +1,8 @@
+for i in range (1, 10):
+    if i <= 5:
+        print("AAAAAHHHHH")
+    elif i >= 6 and i <= 8:
+        print("WHERE ARE ALL THE BRACKETS")
+    else :
+        print("How do you people read this syntax easily")
+             
